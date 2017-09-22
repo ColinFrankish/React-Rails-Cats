1302491d4f4ed8bb7f50f690b3044f842e12f019
Colin Frankish <colin.frankish@gmail.com>
2017-09-22 11:02:03 +0100

    Added Cat model and seeded the db withsome cats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times { Cat.create!(name: "Moggy", mouse_kills: 20) }
+3.times { Cat.create!(name: "Psycho", mouse_kills: 100) }
+3.times { Cat.create!(name: "Sylvester", mouse_kills: 0) }
+3.times { Cat.create!(name: "Oscar", mouse_kills: 20) }
